acf70d5d88e79d3b8fd901cb1cd6a4f3783285a1
qwu1204 <jolesia.wu@gmail.com>
2016-04-25 18:09:08 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // loop
-var count = 0;
+var count = 1;
 var list = '';
 
-while (count < 10) {
+while (count < 11) {
   list += '<li>current count is: ' + count + '</li>';
   count += 1;
 }
@@ -10,3 +10,15 @@ while (count < 10) {
 // output results
 document.write("<h1>Loops</h1>");
 document.write("<ul>" + list + "</ul>");
+
+var myItems = [["question one","answer one"],["question two","answer two"]];
+
+var secondList = "<ul>";
+
+var i = 0;
+while(i < myItems.length){
+ secondList += "<li>" + myItems[i] + "</li>";
+}
+
+secondList += "</ul>";
+document.write("<p>"+myItems[0]+"</p>");
